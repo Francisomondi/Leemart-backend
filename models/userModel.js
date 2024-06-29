01ce4c1503f12b5fea7a4b1bca9cf0e90b59bd1c
Francis Omondi <francisomondi17@gmail.com>
2024-06-29 14:16:55 +0300

    user login,registration

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email:{
+        required:true,
+        type: String,
+        unique: true
+    },
+    password: String,
+    profilePic: String
+},
+{
+    timestamps: true
+})
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const userSignUpController = require('../controller/userSignUp')
+const userSignInController = require('../controller/userSignIn')
+const userDetailsController = require('../controller/userDetails')
+const authToken = require('../middleware/authToken')
+const userSignOut = require('../controller/userSignOut')
+
+const router = express.Router()
+
+
+
+router.post('/signup', userSignUpController)
+router.post('/signin', userSignInController)
+router.get('/user-details', authToken, userDetailsController)
+router.get('/signout', userSignOut)
+
+
+module.exports = router
+
