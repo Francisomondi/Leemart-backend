643fd48239fda609212436e30d5826080da5a551
Francis Omondi <francisomondi17@gmail.com>
2024-08-06 16:12:22 +0300

    search product

--- controller/product/SearchProduct.js
+++ controller/product/SearchProduct.js
@@ -1,7 +1,27 @@
+const productModel = require("../../models/productModel")
+
 const searchProduct = async(req,res)=>{
     try {
         const query = req.query.q
-        console.log(query)
+        const regex = RegExp(query,'i','g')
+        const product = await productModel.find({
+            '$or': [
+                {
+                    productName: regex
+                },
+                {
+                    category: regex
+                }
+            ]
+        })
+
+        res.json({
+            data: product,
+            message: 'Search product list',
+            error: false,
+            success: true
+        })
+
     } catch (error) {
         res.status(400).json({
             message: error.message || error,
