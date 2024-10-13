09bc7bf5fcacb470c85fec5813109100d3388c9c
Francis Omondi <francisomondi17@gmail.com>
2024-10-13 12:49:49 +0300

    callback metadata saved on the database

--- controller/payments/callbackHandler.js
+++ controller/payments/callbackHandler.js
@@ -1,3 +1,5 @@
+const Payment = require("../../models/paymentModel");
+
 const callbackHandler = (req, res) => {
     const callbackData = req.body; // M-Pesa will send the data here
     console.log(callbackData.Body);
@@ -7,7 +9,29 @@ const callbackHandler = (req, res) => {
       res.json('ok')
     }
   
-    console.log(callbackData.Body.stkCallback.CallbackMetadata)
+    //console.log(callbackData.Body.stkCallback.CallbackMetadata)
+
+    
+    const trnx_id = callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value
+    const phone = callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value
+    const amount = callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value
+
+    console.log({phone, amount, trnx_id})
+
+    const payment = new Payment()
+
+    payment.phone = phone
+    payment.amount = amount
+    payment.trnx_id = trnx_id
+
+    payment.save().then((data)=>{
+      console.log({message: 'saved successfully', data})
+    })
+    .catch((error)=>{
+      console.log(error.message)
+  })
+
+
   };
 
   module.exports =  callbackHandler 
